87b72086f4bbeb5f30cfa72f0ce8e0b90eae856d
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-05-11 17:15:31 -0400

    import password interface

--- src/app/auth/interfaces/index.ts
+++ src/app/auth/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './auth-status.enum';
 export * from './check-token.response';
+export * from './forgot-password';
 export * from './register-response';
 export * from './signin-response.interface'
 export * from './user.interface';
