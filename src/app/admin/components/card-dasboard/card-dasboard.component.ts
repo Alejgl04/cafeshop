5602a29feb96f361920cfd6631bfdfaf78dde3db
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-05-25 18:12:41 -0400

    dashboard button route

--- src/app/admin/components/card-dasboard/card-dasboard.component.ts
+++ src/app/admin/components/card-dasboard/card-dasboard.component.ts
@@ -3,11 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { DashboardCount } from '../../interfaces';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card-dasboard',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterModule],
   templateUrl: './card-dasboard.component.html',
   styleUrl: './card-dasboard.component.css'
 })
